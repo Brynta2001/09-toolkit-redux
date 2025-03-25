819925be92af3e91eb28a1642b6a3b48e6feb652
Bryan Tapia <marcelotapia01@outlook.com>
2025-03-25 00:18:40 -0500

    feat: add pokemonSlice

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,13 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+
+      <ul>
+        <li>Test</li>
+        <li>Test</li>
+      </ul>
+    </>
+  );
+};
